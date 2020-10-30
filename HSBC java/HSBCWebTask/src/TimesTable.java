c522d86349f94e5778ecf890c1fb9132025fcd93
Swapnil Holkar <Swapnil@Swapnils-MacBook-Pro.local>
2020-10-30 18:12:40 +0000

    day 5 java and javascript

--- HSBC java/HSBCWebTask/src/TimesTable.java
+++ HSBC java/HSBCWebTask/src/TimesTable.java
@@ -10,32 +10,57 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class FirstServlet
+ * Servlet implementation class TimesTable
  */
-@WebServlet("/FirstServlet")
-public class FirstServlet extends HttpServlet {
+@WebServlet("/swapnil")
+public class TimesTable extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-   
-    public FirstServlet() {
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TimesTable() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		PrintWriter out =response.getWriter();
-		out.write("hello my friends");
+		
+		PrintWriter out=response.getWriter();
+		
+		for(int i=1;i<=10;i++) {
+			
+			out.write(2+" x "+i+" = "+(i*2));
+			out.write("<br>");
+			}
+		
 		out.close();
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		int T=Integer.parseInt(request.getParameter("numT"));
+		
+		for(int i=1;i<=10;i++) {
+			
+			out.write(T+" x "+i+" = "+(i*T));
+			out.write("<br>");
+			}
+		
+		out.close();
+		
+		
+		
 	}
 
 }
